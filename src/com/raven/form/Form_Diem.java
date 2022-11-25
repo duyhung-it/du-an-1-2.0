ac66ab7208f3ec14103ce5eaee8049df130c1d0c
duongn5279 <duonglcph27090@fpt.edu.vn>
2022-11-25 16:21:15 +0700

    cm

--- src/com/raven/form/Form_Diem.java
+++ src/com/raven/form/Form_Diem.java
@@ -7,13 +7,16 @@ package com.raven.form;
 import com.nhomsau.domainmodel.DauDiem;
 import com.nhomsau.domainmodel.DauDiemMon;
 import com.nhomsau.domainmodel.Diem;
+import com.nhomsau.domainmodel.Ky;
 import com.nhomsau.domainmodel.Lop;
 import com.nhomsau.domainmodel.SinhVien;
 import com.nhomsau.repository.impl.DauDiemRepository;
 import com.nhomsau.repository.impl.DauDiemMonRepository;
 import com.nhomsau.repository.impl.DiemRepository;
+import com.nhomsau.repository.impl.KyRepository;
 import com.nhomsau.repository.impl.LopRepository;
 import com.nhomsau.repository.impl.SinhVienRepository;
+import com.nhomsau.viewmodel.QuanLyKy;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -42,32 +45,40 @@ public class Form_Diem extends javax.swing.JPanel {
     SinhVienRepository sinhVienRepository;
     DiemRepository diemRepository;
     DauDiemMonRepository dauDiem_MonRepository;
+    KyRepository kyRepository;
 
     public Form_Diem() throws ParseException {
         initComponents();
         tblDiem.fixTable(jScrollPane1);
+
         lopRepository = new LopRepository();
-        model = (DefaultTableModel) tblDiem.getModel();
-        cbxLop.setLabeText("Lớp");
-        cbxDauDiem.setLabeText("Đầu Điểm");
-        cbBoxModel = (DefaultComboBoxModel<String>) cbxLop.getModel();
-        dauDiemBoxModel = (DefaultComboBoxModel<String>) cbxDauDiem.getModel();
         dauDiemRepository = new DauDiemRepository();
         diemRepository = new DiemRepository();
         dauDiem_MonRepository = new DauDiemMonRepository();
         sinhVienRepository = new SinhVienRepository();
+        kyRepository = new KyRepository();
+
+        model = (DefaultTableModel) tblDiem.getModel();
+        fillAllKy();
         fillAllLop();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String ngayHienTai = dateFormat.format(new Date());
-        Date ngayNhap = dateFormat.parse(ngayHienTai);
+    }
+
+    private void fillAllKy() {
+        cbxKy.setLabeText("Kỳ");
+        List<QuanLyKy> lstKy = kyRepository.findAll();
+        for (QuanLyKy ky : lstKy) {
+            cbBoxModel.addElement(ky.getTen());
+        }
+        cbxLop.setSelectedIndex(-1);
     }
 
     private void fillAllLop() {
+        cbxLop.setLabeText("Lớp");
         List<Lop> lops = lopRepository.getAllLop();
         for (Lop lop : lops) {
             cbBoxModel.addElement(lop.getTenLop());
         }
-        cbxLop.setModel(cbBoxModel);
+        cbxLop.setSelectedIndex(-1);
     }
 
     private void fillAllDauDiem() {
@@ -76,7 +87,6 @@ public class Form_Diem extends javax.swing.JPanel {
         for (DauDiem dauDiem : listDauDiems) {
             dauDiemBoxModel.addElement(dauDiem.getTenDauDiem());
         }
-        cbxDauDiem.setModel(dauDiemBoxModel);
     }
 
     private void loadTable(List<SinhVien> listSinhViens) {
@@ -140,21 +150,21 @@ public class Form_Diem extends javax.swing.JPanel {
 //                    } else {
 //                        JOptionPane.showMessageDialog(this, "Tb1");
 //                    }
-                    JOptionPane.showMessageDialog(this, "không thêm được điểm của SV có maSV = "+maSV +" vì đã tồn tại điểm" );
+                    JOptionPane.showMessageDialog(this, "không thêm được điểm của SV có maSV = " + maSV + " vì đã tồn tại điểm");
                 } else {
                     int insert = diemRepository.saveDiem(d);
                     if (insert == 1) {
-                        JOptionPane.showMessageDialog(this, "Thêm điểm cho SV có MaSV = "+maSV + "Thành Công");
+                        JOptionPane.showMessageDialog(this, "Thêm điểm cho SV có MaSV = " + maSV + "Thành Công");
                     } else {
-                        JOptionPane.showMessageDialog(this, "Thêm điểm cho SV có MaSV = "+maSV + "Thất bại");
+                        JOptionPane.showMessageDialog(this, "Thêm điểm cho SV có MaSV = " + maSV + "Thất bại");
                     }
                 }
             }
         }
         loadTable(dsSV);
     }
-    
-        private void update() {
+
+    private void update() {
         boolean validate = true;
         int rowCount = model.getRowCount();
         String dauDiem = (String) cbxDauDiem.getSelectedItem();
@@ -195,12 +205,12 @@ public class Form_Diem extends javax.swing.JPanel {
                 if (checkSV == 1) {
                     int update = diemRepository.updateDiem(d);
                     if (update == 1) {
-                        JOptionPane.showMessageDialog(this, "Sửa điểm thành công cho SV có maSV = "+maSV);
+                        JOptionPane.showMessageDialog(this, "Sửa điểm thành công cho SV có maSV = " + maSV);
                     } else {
-                        JOptionPane.showMessageDialog(this, "Sửa điểm thất bại cho SV có maSV = "+maSV);
+                        JOptionPane.showMessageDialog(this, "Sửa điểm thất bại cho SV có maSV = " + maSV);
                     }
                 } else {
-                    JOptionPane.showMessageDialog(this,"Sửa không thành công vì SV có maSV = "+maSV + " chưa có điểm");
+                    JOptionPane.showMessageDialog(this, "Sửa không thành công vì SV có maSV = " + maSV + " chưa có điểm");
                 }
             }
         }
@@ -217,42 +227,30 @@ public class Form_Diem extends javax.swing.JPanel {
     private void initComponents() {
 
         panelTransparent1 = new com.raven.swing.PanelTransparent();
-        cbxLop = new com.raven.swing.combobox.Combobox();
-        cbxDauDiem = new com.raven.swing.combobox.Combobox();
         panelTransparent2 = new com.raven.swing.PanelTransparent();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblDiem = new com.raven.swing.table.Table();
         btnSave = new com.raven.swing.button.Button();
         button1 = new com.raven.swing.button.Button();
+        panelTransparent3 = new com.raven.swing.PanelTransparent();
+        cbxLop = new com.raven.swing.combobox.Combobox();
+        cbxMon = new com.raven.swing.combobox.Combobox();
+        cbxKy = new com.raven.swing.combobox.Combobox();
 
         setPreferredSize(new java.awt.Dimension(1027, 920));
 
-        cbxLop.setToolTipText("Lớp");
-        cbxLop.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                cbxLopItemStateChanged(evt);
-            }
-        });
-
-        cbxDauDiem.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                cbxDauDiemItemStateChanged(evt);
-            }
-        });
-
         panelTransparent2.setOpaque(true);
 
-        tblDiem.setBackground(new java.awt.Color(255, 255, 255));
         tblDiem.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "STT", "Mã", "Ho Ten", "Đầu điểm", "Hệ số", "Điểm"
+                "STT", "Mã", "Ho Ten"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, true
+                false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -299,7 +297,8 @@ public class Form_Diem extends javax.swing.JPanel {
         panelTransparent2Layout.setVerticalGroup(
             panelTransparent2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelTransparent2Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 412, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(panelTransparent2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -307,30 +306,56 @@ public class Form_Diem extends javax.swing.JPanel {
                 .addContainerGap())
         );
 
+        cbxLop.setToolTipText("Lớp");
+        cbxLop.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cbxLopItemStateChanged(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panelTransparent3Layout = new javax.swing.GroupLayout(panelTransparent3);
+        panelTransparent3.setLayout(panelTransparent3Layout);
+        panelTransparent3Layout.setHorizontalGroup(
+            panelTransparent3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelTransparent3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(cbxKy, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(cbxMon, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(143, 143, 143)
+                .addComponent(cbxLop, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        panelTransparent3Layout.setVerticalGroup(
+            panelTransparent3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelTransparent3Layout.createSequentialGroup()
+                .addContainerGap(17, Short.MAX_VALUE)
+                .addGroup(panelTransparent3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cbxLop, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cbxMon, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cbxKy, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(17, 17, 17))
+        );
+
         javax.swing.GroupLayout panelTransparent1Layout = new javax.swing.GroupLayout(panelTransparent1);
         panelTransparent1.setLayout(panelTransparent1Layout);
         panelTransparent1Layout.setHorizontalGroup(
             panelTransparent1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelTransparent1Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(panelTransparent1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(panelTransparent3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(panelTransparent1Layout.createSequentialGroup()
+                        .addContainerGap()
                         .addComponent(panelTransparent2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(panelTransparent1Layout.createSequentialGroup()
-                        .addComponent(cbxDauDiem, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(cbxLop, javax.swing.GroupLayout.PREFERRED_SIZE, 373, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(48, 48, 48))))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         panelTransparent1Layout.setVerticalGroup(
             panelTransparent1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelTransparent1Layout.createSequentialGroup()
-                .addGap(21, 21, 21)
-                .addGroup(panelTransparent1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cbxLop, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(cbxDauDiem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addContainerGap()
+                .addComponent(panelTransparent3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(panelTransparent2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -369,17 +394,6 @@ public class Form_Diem extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_tblDiemInputMethodTextChanged
 
-    private void cbxDauDiemItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cbxDauDiemItemStateChanged
-        // TODO add your handling code here:
-        String tenLop = (String) cbxLop.getSelectedItem();
-        Lop lop = lopRepository.getLop(tenLop);
-        String tenDauDiem = (String) cbxDauDiem.getSelectedItem();
-        if (tenDauDiem != null) {
-            loadTable(sinhVienRepository.getSinhViens(lop.getId()));
-        }
-
-    }//GEN-LAST:event_cbxDauDiemItemStateChanged
-
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
         // TODO add your handling code here:
         save();
@@ -394,11 +408,13 @@ public class Form_Diem extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.raven.swing.button.Button btnSave;
     private com.raven.swing.button.Button button1;
-    private com.raven.swing.combobox.Combobox cbxDauDiem;
+    private com.raven.swing.combobox.Combobox cbxKy;
     private com.raven.swing.combobox.Combobox cbxLop;
+    private com.raven.swing.combobox.Combobox cbxMon;
     private javax.swing.JScrollPane jScrollPane1;
     private com.raven.swing.PanelTransparent panelTransparent1;
     private com.raven.swing.PanelTransparent panelTransparent2;
+    private com.raven.swing.PanelTransparent panelTransparent3;
     private com.raven.swing.table.Table tblDiem;
     // End of variables declaration//GEN-END:variables
 }
